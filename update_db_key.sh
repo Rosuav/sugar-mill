7bc1570451906bdb171dd5577522bb0f74d3506a
Chris Angelico <rosuav@gmail.com>
2026-03-23 11:14:10 +1100

    Refactor deployment into a separate (shared) script

--- update_db_key.sh
+++ update_db_key.sh
@@ -1,12 +1,8 @@
-# Have this script on Gideon in /root/sugar-mill eg by cloning the repo
+cd `dirname $0`
 exec 3<>db.rosuav.com.pem
 while read line; do
 	[ "$line" == '-----BEGIN PRIVATE KEY-----' ] && exec 3>&- && exec 3<>db.rosuav.com.key
 	echo $line >&3
 done
 exec 3>&-
-cp db.rosuav.com.* /home/rosuav/stillebot/
-chown rosuav: /home/rosuav/stillebot/db.rosuav.com.*
-cp db.rosuav.com.* /etc/postgresql/16/main/
-chown postgres: /etc/postgresql/16/main/db.rosuav.com.*
-systemctl reload postgresql
+./deploy
