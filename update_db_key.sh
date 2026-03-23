e4978ab3b8de4f59e43db539b59cde63ef52b774
Chris Angelico <rosuav@gmail.com>
2026-03-23 11:15:47 +1100

    Using traditional key format

--- update_db_key.sh
+++ update_db_key.sh
@@ -1,7 +1,7 @@
 cd `dirname $0`
 exec 3<>db.rosuav.com.pem
 while read line; do
-	[ "$line" == '-----BEGIN PRIVATE KEY-----' ] && exec 3>&- && exec 3<>db.rosuav.com.key
+	[ "$line" == '-----BEGIN RSA PRIVATE KEY-----' ] && exec 3>&- && exec 3<>db.rosuav.com.key
 	echo $line >&3
 done
 exec 3>&-
